4683c2756d7190d2e311f6ef6ac1a457575916e2
Seloro <lorenzocupeirosebastian@gmail.com>
2025-10-15 17:53:29 -0300

    Preparación de carpeta propia

---
+++ Mutant-Escape/Assets/Sebastián/Codigos/ControlJugador.cs
@@ -0,0 +1,105 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+    [Header("Movimiento Jugador")]
+    public Transform jugador;
+    public float velocidad = 5f;
+    public float velocidadRotacion = 10f;
+    public float fuerzaDeSalto = 5f;
+
+    [Header("Movimiento Camara")]
+    public Transform transformCamara;
+    public float distanciaCamara = 5f;
+    public float sencibilidadCamara = 5f;
+    private float rotacionYcam, rotacionXcam;
+
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rotacionYcam = transform.eulerAngles.y;
+        rotacionXcam = transform.eulerAngles.x;
+
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+
+        MovimientoJugador();
+        Saltar();
+
+        MovimientoCamara();
+
+        ResetearPosision();
+    }
+
+    private void MovimientoCamara()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        rotacionYcam += mouseX * sencibilidadCamara;
+        rotacionXcam += mouseY * sencibilidadCamara;
+
+        rotacionXcam = Mathf.Clamp(rotacionXcam, -89f, 89f);
+
+        Quaternion rotacionCamara = Quaternion.Euler(rotacionXcam, rotacionYcam, 0f);
+        Vector3 offset = rotacionCamara * new Vector3(0f, 0f, -distanciaCamara);
+
+        transformCamara.position = transform.position + offset;
+        transformCamara.LookAt(transform.position);
+    }
+
+    private void MovimientoJugador()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 direccionInput = new Vector3(h, 0f, v).normalized;
+
+        Quaternion rotacionJugador = Quaternion.Euler(0f, rotacionYcam, 0f);
+        Vector3 direccion = rotacionJugador * direccionInput;
+
+        Vector3 velocidadDireccionada = direccion * velocidad;
+        rb.velocity = new Vector3(velocidadDireccionada.x, rb.velocity.y, velocidadDireccionada.z);
+
+        if (direccionInput.sqrMagnitude > 0.01f)
+        {
+            Quaternion anguloObjetivo = Quaternion.LookRotation(direccion, Vector3.up);
+            Quaternion rotacion = Quaternion.Slerp(rb.rotation, anguloObjetivo, velocidadRotacion * Time.deltaTime);
+            rb.MoveRotation(rotacion);
+        }
+    }
+
+    void ResetearPosision()
+    {
+        if (transform.position.y < -10)
+        {
+            transform.position = Vector3.up * 2;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    void Saltar()
+    {
+        if (Input.GetButtonDown("Saltar") && ComprobacioSuelo())
+            rb.velocity += Vector3.up * fuerzaDeSalto;
+    }
+
+    bool ComprobacioSuelo()
+    {
+        return Physics.Raycast(jugador.position, Vector3.down, 1.55f);
+    }
+
+    public void Disparo()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Physics.Raycast(jugador.position, Vector3.forward, 10f);
+        }
+    }
+}
