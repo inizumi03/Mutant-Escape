f19c4b854061f58104a7c9a1968cf73c338e9574
abril <abrilludmilacorrea@gmail.com>
2025-11-09 21:56:24 -0300

    Arreglo menú.

--- Mutant-Escape/Assets/Abril/Menu.cs
+++ Mutant-Escape/Assets/Abril/Menu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public GameObject instruccionesPanel;
+    public GameObject CreditosPanel;
     public GameObject MenuPanel;
     public string sceneName;
     public void Jugar()
@@ -18,13 +19,22 @@ public class Menu : MonoBehaviour
         MenuPanel.SetActive(false);
         instruccionesPanel.SetActive(true);
     }
-
     public void CerrarInstrucciones()
     {
         MenuPanel.SetActive(true);
         instruccionesPanel.SetActive(false);
     }
 
+    public void VerCreditos()
+    {
+        MenuPanel.SetActive(false);
+        CreditosPanel.SetActive(true);
+    }
+    public void CerrarCreditos()
+    {
+        MenuPanel.SetActive(true);
+        CreditosPanel.SetActive(false);
+    }
     public void Salir()
     {
         Application.Quit();
