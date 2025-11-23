d9fe0df8586de6e3056afa860ae4e97d8de40269
Seloro <lorenzocupeirosebastian@gmail.com>
2025-11-23 20:53:00 -0300

    Mejora de mechero

--- Mutant-Escape/Assets/Sebastián/Codigos/Trampa.cs
+++ Mutant-Escape/Assets/Sebastián/Codigos/Trampa.cs
@@ -7,17 +7,42 @@ public class Trampa : MonoBehaviour
     [Header("Valores")]
     public int daño;
     public int indice;
+    public float intervalo;
 
     public delegate void EfectoDeTrampa(int daño, int indice);
     static public event EfectoDeTrampa Activar;
 
+    float temp;
+
+    private void Awake()
+    {
+        temp = intervalo;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Jugador"))
+        if (other.gameObject.CompareTag("Jugador") && indice != 2)
         {
             Activar.Invoke(daño, indice);
             if (indice != 1)
                 gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("1");
+        if (other.gameObject.CompareTag("Jugador") && indice == 2)
+        {
+            print("2");
+
+            temp -= Time.deltaTime;
+
+            if (temp < 0)
+            {
+                temp = intervalo;
+                Activar.Invoke(daño, 0);
+            }
         }
     }
 }
